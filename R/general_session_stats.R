8fa9038068078fce83f1bb8b2936afb106ffd98a
Nelson Roque <nelsonroquejr@gmail.com>
2021-01-15 18:23:14 -0500

    more stats

--- R/general_session_stats.R
+++ R/general_session_stats.R
@@ -36,6 +36,8 @@ general_session_stats <- function(.data, group_var, lag_n = 10, sim="drivesafety
                 max_vehicle_y = max(SubjectY, na.rm=T),
 
                 # vehicle velocity ----- (get units from DS codebook)
+                mean_velocity = mean(Velocity, na.rm=T),
+                median_velocity = median(Velocity, na.rm=T),
                 p10_velocity = quantile(Velocity, probs=0.1, na.rm=T),
                 p30_velocity = quantile(Velocity, probs=0.3, na.rm=T),
                 p70_velocity = quantile(Velocity, probs=0.7, na.rm=T),
@@ -44,6 +46,8 @@ general_session_stats <- function(.data, group_var, lag_n = 10, sim="drivesafety
                 # sum_velocity_gte_1 = sum(Velocity > 0 & Velocity <= 1, na.rm=T),# <--- maybe some sum scores?
 
                 # vehicle acceleration ----- (get units from DS codebook)
+                mean_accel = mean(Accel, na.rm=T),
+                median_accel = median(Accel, na.rm=T),
                 p10_accel = quantile(Accel, probs=0.1, na.rm=T),
                 p30_accel = quantile(Accel, probs=0.3, na.rm=T),
                 p70_accel = quantile(Accel, probs=0.7, na.rm=T),
@@ -51,6 +55,8 @@ general_session_stats <- function(.data, group_var, lag_n = 10, sim="drivesafety
                 sd_accel = sd(Accel, na.rm=T),
 
                 # vehicle lane position ----- (get units from DS codebook)
+                mean_lanepos = mean(LanePos_recode, na.rm=T),
+                median_lanepos = median(LanePos_recode, na.rm=T),
                 p10_lanepos = quantile(LanePos_recode, probs=0.1, na.rm=T),
                 p30_lanepos = quantile(LanePos_recode, probs=0.3, na.rm=T),
                 p70_lanepos = quantile(LanePos_recode, probs=0.7, na.rm=T),
@@ -58,6 +64,8 @@ general_session_stats <- function(.data, group_var, lag_n = 10, sim="drivesafety
                 sd_lanepos = sd(LanePos_recode, na.rm=T),
 
                 # vehicle steering angle ----- (get units from DS codebook)
+                mean_steer = mean(Steer, na.rm=T),
+                median_steer = median(Steer, na.rm=T),
                 p10_steer = quantile(Steer, probs=0.1, na.rm=T),
                 p30_steer = quantile(Steer, probs=0.3, na.rm=T),
                 p70_steer = quantile(Steer, probs=0.7, na.rm=T),
@@ -65,6 +73,8 @@ general_session_stats <- function(.data, group_var, lag_n = 10, sim="drivesafety
                 sd_steer = sd(Steer, na.rm=T),
 
                 # vehicle lataccel ----- (get units from DS codebook)
+                mean_lataccel = mean(LatAccel, na.rm=T),
+                median_lataccel = median(LatAccel, na.rm=T),
                 p10_lataccel = quantile(LatAccel, probs=0.1, na.rm=T),
                 p30_lataccel = quantile(LatAccel, probs=0.3, na.rm=T),
                 p70_lataccel = quantile(LatAccel, probs=0.7, na.rm=T),
@@ -72,6 +82,8 @@ general_session_stats <- function(.data, group_var, lag_n = 10, sim="drivesafety
                 sd_lataccel = sd(LatAccel, na.rm=T),
 
                 # vehicle brake ----- (get units from DS codebook)
+                mean_brake = mean(Brake, na.rm=T),
+                median_brake = median(Brake, na.rm=T),
                 p10_brake = quantile(Brake, probs=0.1, na.rm=T),
                 p30_brake = quantile(Brake, probs=0.3, na.rm=T),
                 p70_brake = quantile(Brake, probs=0.7, na.rm=T),
@@ -83,7 +95,8 @@ general_session_stats <- function(.data, group_var, lag_n = 10, sim="drivesafety
 
                 collision_events = paste0(unique(Collision))) %>%
       # create flags if certain critical data outliers exist -----
-      mutate(flag_session_0_0_origin = ifelse(min_vehicle_x == 0 & min_vehicle_y == 0, T, F)) %>%
+      mutate(flag_session_0_0_origin = ifelse(min_vehicle_x == 0 & min_vehicle_y == 0, T, F),
+             flag_if_collision = ifelse(!is.na(collision_events), T, F)) %>%
       mutate(param_fps_assumption = fps_assumption)
   }
 }
